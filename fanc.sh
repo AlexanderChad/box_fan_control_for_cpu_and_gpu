227343326de4c29e2d677da6fe46a93d16d062b2
Alexander.Chad <Alexander.Chad@yandex.ru>
2024-06-22 18:05:06 +0700

    добавлены проверки: * на возврат вместо температуры пустой строки * на аномальные значения температуры * нулевые значения кол-ва устройств Добавлен высокий приоритет для сервиса. Небольшая оптимизация (пара сравнений вместо вычислений).

--- fanc.sh
+++ fanc.sh
@@ -7,14 +7,41 @@ GPU_MIN_TEMP=50
 GPU_MAX_TEMP=65
 FAN_MIN_SIGNAL=75
 
+ANOMAL_MIN_TEMP=5
+ANOMAL_MAX_TEMP=90
+
 CPU_SLOT_NUM=`grep "physical id"  /proc/cpuinfo | sort -u | wc -l`
 GPU_CARDS_NUM=`nvidia-smi -L | wc -l`
+
+FAN_CTRL_PATH='/sys/devices/platform/nct6775.2592/hwmon/[[:print:]]*'
 
 echo "CPU_SLOT_NUM = $CPU_SLOT_NUM, GPU_CARDS_NUM = $GPU_CARDS_NUM"
 
 echo "Enable manual PWM control"
-echo 1 > /sys/devices/platform/nct6775.2592/hwmon/[[:print:]]*/pwm1_enable
-echo 1 > /sys/devices/platform/nct6775.2592/hwmon/[[:print:]]*/pwm2_enable
+echo 1 > $FAN_CTRL_PATH/pwm1_enable
+echo 1 > $FAN_CTRL_PATH/pwm2_enable
+
+#Set the fan PWM value
+function set_fan_pwm_val {
+	echo $1 > $FAN_CTRL_PATH/pwm1
+	echo $1 > $FAN_CTRL_PATH/pwm2
+}
+
+#Checking CPU devices
+if [ -z "$CPU_SLOT_NUM" ] || ((CPU_SLOT_NUM==0)); then
+	echo "ERROR! No CPU devices found."
+	set_fan_pwm_val 255
+	while true
+	do
+	sleep $DELAY
+	done
+fi
+
+#Checking GPU devices
+if [ -z "$GPU_CARDS_NUM" ]; then
+	echo "WARNING! No GPU devices found."
+	GPU_CARDS_NUM=0
+fi
 
 #Returns the maximum value.
 function get_max_val {
@@ -28,7 +55,7 @@ local dev_slots=$(($1?$GPU_CARDS_NUM:$CPU_SLOT_NUM))
 for ((i=0; i<$dev_slots; i++))
 do
 	local gtemp=$(($1?$(nvidia-smi -i $i --query-gpu=temperature.gpu --format=csv,noheader):$(sensors coretemp-isa-000$i | grep "Package id $i" | sed -r 's/\..+//;s|.*\+||')))
-	if [ -z "$gtemp" ]; then
+	if [ -z "$gtemp" ] || (($gtemp<$ANOMAL_MIN_TEMP)) || (($gtemp>$ANOMAL_MAX_TEMP)); then
 		gtemp=$(($1?$GPU_MAX_TEMP:$CPU_MAX_TEMP))
 	fi
 	g_max_temp=$(get_max_val $g_max_temp $gtemp)
@@ -49,33 +76,39 @@ fi
 
 echo "Start PWM control task"
 
+cpu_max_temp_val_old=$CPU_MIN_TEMP
+gpu_max_temp_val_old=$GPU_MIN_TEMP
+cpu_new_pwm_val=$FAN_MIN_SIGNAL
+gpu_new_pwm_val=$FAN_MIN_SIGNAL
+
 while true
 do
-#echo "Checking temperatures..."
-#echo "$(date +"%d/%m/%y %T")"
 
+#Get CPU and GPU temperatures
 cpu_max_temp_val=$(get_dev_max_temp 0)
 gpu_max_temp_val=$(get_dev_max_temp 1)
 
-#echo "cpu_max_temp_val = $cpu_max_temp_val"
-#echo "gpu_max_temp_val = $gpu_max_temp_val"
+#Checking changes for CPU and calc new pwm values
+if ((cpu_max_temp_val!=cpu_max_temp_val_old)); then
+	cpu_max_temp_val_old=$cpu_max_temp_val
+	cpu_new_pwm_val=$(calc_new_pwm_val $cpu_max_temp_val $CPU_MIN_TEMP $CPU_MAX_TEMP)
+fi
 
-cpu_new_pwm_val=$(calc_new_pwm_val $cpu_max_temp_val $CPU_MIN_TEMP $CPU_MAX_TEMP)
-gpu_new_pwm_val=$(calc_new_pwm_val $gpu_max_temp_val $GPU_MIN_TEMP $GPU_MAX_TEMP)
-
-#echo "cpu_new_pwm_val = $cpu_new_pwm_val"
-#echo "gpu_new_pwm_val = $gpu_new_pwm_val"
+#Checking changes for GPU and calc new pwm values
+if ((gpu_new_pwm_val!=gpu_max_temp_val_old)); then
+	gpu_max_temp_val_old=$gpu_max_temp_val
+	gpu_new_pwm_val=$(calc_new_pwm_val $gpu_max_temp_val $GPU_MIN_TEMP $GPU_MAX_TEMP)
+fi
 
 case_fan_new_speed=$(get_max_val $cpu_new_pwm_val $gpu_new_pwm_val)
-case_fan_old_speed=$(cat /sys/devices/platform/nct6775.2592/hwmon/[[:print:]]*/pwm1)
+case_fan_now_speed=$(cat $FAN_CTRL_PATH/pwm1)
 
-if (($case_fan_old_speed!=$case_fan_new_speed)); then
+if (($case_fan_now_speed!=$case_fan_new_speed)); then
 	echo "$(date +"%d/%m/%y %T") Checking temperatures..."
 	echo "cpu_max_temp_val = $cpu_max_temp_val"
 	echo "gpu_max_temp_val = $gpu_max_temp_val"
-	echo "PWM: $case_fan_old_speed -> $case_fan_new_speed"
-	echo $case_fan_new_speed > /sys/devices/platform/nct6775.2592/hwmon/[[:print:]]*/pwm1
-	echo $case_fan_new_speed > /sys/devices/platform/nct6775.2592/hwmon/[[:print:]]*/pwm2
+	echo "PWM: $case_fan_now_speed -> $case_fan_new_speed"
+	set_fan_pwm_val $case_fan_new_speed
 fi
 
 sleep $DELAY
